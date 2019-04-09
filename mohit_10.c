c8546f1be9823dbeb3b074e4322749be441d02e6
mohit15sharma <49458710+mohit15sharma@users.noreply.github.com>
2019-04-10 02:58:49 +0530

    Create mohit_10.c

---
+++ mohit_10.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+main(){
+	
+	int distance_covered=0 , cur_pos=0 , prev_pos=0;
+	bool flag = true;
+	
+	while(flag)
+		{
+			cur_pos=143;
+			prev_pos=125;
+			int my_queue[] = {86, 1470, 913, 1774, 948, 1509, 1022, 1750, 130};
+			
+			prev_pos = cur_pos;
+			for(int i=0;i<9;i++)
+                        {
+				cur_pos = my_queue[i];
+				distance_covered = distance_covered + abs(cur_pos-prev_pos);
+				prev_pos = cur_pos;
+			}
+			
+			printf("\nThe total cylinders covered are : %d",distance_covered);
+			flag = false;
+		}
+}
