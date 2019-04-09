2c6506ce8ef87e1296c94fc4b15f414b78c8d756
mohit15sharma <49458710+mohit15sharma@users.noreply.github.com>
2019-04-10 03:11:30 +0530

    Update mohit_10.c

--- mohit_10.c
+++ mohit_10.c
@@ -1,23 +1,23 @@
 #include<stdio.h>
 #include<math.h>
 
-main(){
-	
-	int distance_covered=0 , cur_pos=0 , prev_pos=0;
+main()
+{
+	int distance_covered=0 , current_position=0 , previous_position=0;
 	bool flag = true;
 	
 	while(flag)
 		{
-			cur_pos=143;
-			prev_pos=125;
-			int my_queue[] = {86, 1470, 913, 1774, 948, 1509, 1022, 1750, 130};
+			current_position=143;
+			previous_position=125;
+			int queue[] = {86, 1470, 913, 1774, 948, 1509, 1022, 1750, 130};
 			
-			prev_pos = cur_pos;
+			previous_position = current_position;
 			for(int i=0;i<9;i++)
                         {
-				cur_pos = my_queue[i];
-				distance_covered = distance_covered + abs(cur_pos-prev_pos);
-				prev_pos = cur_pos;
+				current_position = queue[i];
+				distance_covered = distance_covered + abs(current_position-previous_position);
+				previous_position = current_position;
 			}
 			
 			printf("\nThe total cylinders covered are : %d",distance_covered);
