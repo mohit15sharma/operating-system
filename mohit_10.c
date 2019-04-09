47f472ebace3868ba27ee5aae585a103f8e5e142
mohit15sharma <49458710+mohit15sharma@users.noreply.github.com>
2019-04-10 03:03:11 +0530

    Update mohit_10.c

--- mohit_10.c
+++ mohit_10.c
@@ -9,10 +9,10 @@ main(){
 		{
 			cur_pos=143;
 			prev_pos=125;
-			int my_queue[] = {86, 1470, 913, 1774, 948, 1509, 1022, 1750, 130};
+			int my_queue[] = {86, 1470, 913, 1774, 948, 1509, 1750, 130};
 			
 			prev_pos = cur_pos;
-			for(int i=0;i<9;i++)
+			for(int i=0;i<8;i++)
                         {
 				cur_pos = my_queue[i];
 				distance_covered = distance_covered + abs(cur_pos-prev_pos);
